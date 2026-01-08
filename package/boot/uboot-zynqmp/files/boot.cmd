d73c4f19f58a8e3e0650dd58ed377227ef669b66
Robbe Gaeremynck <robbe.gaeremynck@outlook.com>
2026-01-08 16:02:27 +0100

    Add boot.cmd for zynqmp

---
+++ package/boot/uboot-zynqmp/files/boot.cmd
@@ -0,0 +1,12 @@
+setenv devtype mmc
+setenv bootpart 0:1
+
+setenv kernel_file fit.itb
+setenv kernel_loadaddr 0x04000000
+
+setenv bootargs "console=ttyPS0,115200n8 root=/dev/mmcblk0p2 rootwait earlyprintk"
+
+setenv default_openwrt_boot "load ${devtype} ${bootpart} ${kernel_loadaddr} ${kernel_file};bootm ${kernel_loadaddr};"
+setenv bootcmd "run default_openwrt_boot"
+
+boot
